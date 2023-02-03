6bd5a79f5da23a7add59e8c2be3629899c97863e
Jacob Cheatley (AWARE) <jacob.cheatley@awaregroup.com>
2023-02-04 11:07:15 +1300

    Symbols

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import json
+import re
 from typing import Dict, List
 
 import pandas as pd
@@ -6,16 +7,30 @@ import pandas as pd
 with open("settings.json") as f:
     settings = json.load(f)
 
+symbol_pattern = re.compile(r"\[(.)\]")
+symbol_render = r'<img class="symbol" src="/symbols/symbol-\1.png" />'
 
-def get_card_data(use_print_count=True) -> List[Dict]:
+
+def replace_symbols(s: str) -> str:
+    return symbol_pattern.sub(symbol_render, s)
+
+
+def get_card_data(use_print_count: bool = True) -> List[Dict]:
     google_sheets_url = f'https://docs.google.com/spreadsheets/d/{settings["google_sheets_id"]}/gviz/tq?tqx=out:csv&sheet={settings["google_sheets_card_worksheet"]}'
     df = pd.read_csv(google_sheets_url)
     # Duplicate by print_count
     if use_print_count:
         df = df.loc[df.index.repeat(df.print_count)]
     df = df.drop(columns=["print_count"])
+
+    # Symbol substitution
+    df["cost"] = df["cost"].apply(lambda n: replace_symbols(n * "[W]"))
+    df["mulch"] = df["mulch"].apply(lambda n: replace_symbols(n * "[N]"))
+    df["effect"] = df["effect"].apply(replace_symbols)
+
     return df.to_dict(orient="records")
 
 
 if __name__ == "__main__":
-    print(get_card_data())
+    # print(get_card_data())
+    print(replace_symbols("[W][N]"))
